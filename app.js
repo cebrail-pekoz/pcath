d6fdf2b729f71529e2b268c23c60033d886e7aa9
cebrail-pekoz <pkzcbrl@gmail.com>
2022-10-27 18:04:29 +0300

    ejs

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 // ***** Modüller
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 // ***** //Modüller
 
 const app = express(); // express uygulaması
 app.use(express.static('public')); // statik dosyalar -middleware
 
+// ***** Template Engine
+app.set('view engine', 'ejs');
+// ***** //Template Engine
+
 // ***** Yönlendirmeler
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
 });
 // ***** //Yönlendirmeler
 
