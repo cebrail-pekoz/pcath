99ef4d411e0eeec4772e20d2fa04c2fdd127af0c
cebrail-pekoz <pkzcbrl@gmail.com>
2022-10-27 15:06:02 +0300

    iki

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-    console.log('Fonksiyon Çalıştı');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Merhaba');
+});
+
+// ***** Server Çalıştırma
+const port = 3000;
+const localHost = '127.0.0.1';
+app.listen(port, () => {
+    console.log(`Server Çalışıyor >>> http://${localHost}:${port}`);
+});
+// ***** //Server Çalıştırma
